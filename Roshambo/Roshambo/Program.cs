c0337473d466d4af4901bed89590ea35ad895a39
ktMcD <geek.katie@gmail.com>
2022-11-03 19:41:03 -0700

    Initial commit for Roshambo

---
+++ Roshambo/Roshambo/Program.cs
@@ -0,0 +1,17 @@
+﻿/* katie mcdougall
+ * test driver for Roshambo
+ * 11/3/22
+*/
+
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+
+namespace Roshambo
+{
+    internal class Program : Roshambo
+    {
+        Program TestRoshambo = new Program();
+        // 
+       
+    }
+}
