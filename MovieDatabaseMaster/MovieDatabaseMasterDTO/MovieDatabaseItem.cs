754c891d572542fa8365cfa4157fd498e1037df0
ktMcD <geek.katie@gmail.com>
2022-12-15 05:22:15 -0800

    movie database first commit
    plus html detritus

---
+++ MovieDatabaseMaster/MovieDatabaseMasterDTO/MovieDatabaseItem.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieDatabaseMasterDTO
+{
+    public class MovieDatabaseItem
+    {
+        public int Id { get; set; }
+        public string MovieTitle { get; set; }
+        public string? Genre { get; set; }
+        public float Runtime { get; set; }
+    }
+}
---
+++ MovieDatabaseMaster/MovieDatabaseMasterRepository/MovieDatabaseItemRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace MovieDatabaseMasterRepository
+{
+    public class MovieDatabaseItemRepository
+    {
+        private IConfigurationRoot _configuration;
+        private DbContextOptionsBuilder<ApplicationDbContext> _optionsBuilder;
+        public MovieDatabaseItemRepository()
+        {
+            BuildOptions();
+        }
+        private void BuildOptions()
+        {
+            _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+            _optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("MovieDbManager"));
+        }
+
+    }
+}
