fda7a164382fdc3e34741b74a97983dcd5104b83
ktMcD <geek.katie@gmail.com>
2023-01-09 18:17:26 -0800

    Jan 4ht excercises

---
+++ MyWebApp/MyWebApp/StudentRepository.cs
@@ -0,0 +1,28 @@
+﻿using MyWebApp.Models;
+
+namespace MyWebApp
+{
+    public class StudentRepository
+    {
+        private List<StudentViewModel> _mockStudents;
+
+        public StudentRepository()
+        {
+            CreateMockStudentList();
+        }
+        private void CreateMockStudentList()
+        {
+            _mockStudents = new List<StudentViewModel>();
+            _mockStudents.Add(new StudentViewModel { Id = 1, Name = "Steve", Course = "AHBC C#" });
+            _mockStudents.Add(new StudentViewModel { Id = 2, Name = "Mary", Course = "AHBC C#" });
+            _mockStudents.Add(new StudentViewModel { Id = 3, Name = "David", Course = "AHBC C#" });
+            _mockStudents.Add(new StudentViewModel { Id = 4, Name = "Kate", Course = "AHBC C#" });
+        }
+        public IEnumerable<StudentViewModel> GetMockStudents()
+        {
+            return _mockStudents;
+        }
+
+    }
+
+}
