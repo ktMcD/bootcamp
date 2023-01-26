db575c7af1c606125994411ea4fc461cfbab5b95
ktMcD <geek.katie@gmail.com>
2023-01-26 15:41:18 -0800

    trying to get create to work

--- MVC_with_EF2/MVC_with_EF2/Controllers/StudentController.cs
+++ MVC_with_EF2/MVC_with_EF2/Controllers/StudentController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MVC_with_EF2.DataAccessLayer;
+using MVC_with_EF2.Models;
+using System.Collections.Generic;
 
 namespace MVC_with_EF2.Controllers
 {
@@ -11,5 +14,63 @@ namespace MVC_with_EF2.Controllers
         {
             return View(db.Students.ToList());
         }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                throw new BadHttpRequestException("No Id Provided");
+            }
+            Student student = db.Students
+                .Where(x => x.ID == id)
+                .Include(i => i.Enrollments) // <<<<<-------- pay attention to this use it if you want to use a foreign key
+                .FirstOrDefault();
+
+            if (student == null)
+            {
+                throw new BadHttpRequestException("Id not found");
+            }
+
+            for (int i = 0; i < student.Enrollments.Count; i++)
+            {
+                student.Enrollments[i].Course = db.Courses.FirstOrDefault(x => x.CourseID == student.Enrollments[i].CourseID);
+            }
+
+            return View(student); // this is the name of the controller and its matching view folder
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public Student Create(IFormCollection collection)
+        {
+            try
+            {
+                Student newStudent = new Student()
+                {
+                    LastName = collection["Name"],
+                    FirstMidName = collection["Course"],
+                    EnrollmentDate = DateTime.Parse(collection["EnrollmentDate"])
+                };
+                newStudent.ID = db.Students.Max(x => x.ID) + 1;
+                db.Students.Add(newStudent);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Student));
+
+            }
+
+            catch
+            {
+                return View(Index);
+            }
+
+        }
+
     }
 }
