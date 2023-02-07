6ab8b04c0ae03636c4a2d1f3d51ace323263d2bd
ktMcD <geek.katie@gmail.com>
2023-02-07 12:22:02 -0800

    countries are set up - now it's time to do the if/elses

--- CountriesOfTheWorld/country.js
+++ CountriesOfTheWorld/country.js
@@ -6,24 +6,49 @@ class Country {
         this.colors = colors;
     }
 }
-let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"]);
-let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
-let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
 
+let canada = new Country("Canada", "French and English", "Hello, World, eh?", ["red", "white", "blue"]);
+let finland = new Country("Finland", "Finnish", "'Sup", ["blue", "yellow", "red"])
+let haiti = new Country("Haiti", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
+let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
+let rwanda = new Country("Rwanda", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
+let uganda = new Country("Uganda", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
+let ukraine = new Country("Ukraine", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
+let venezuela = new Country("Venezuela", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
 
 function SwitchCountry() {
-    let input = "USA";
+    let input = document.querySelector('#CountryList').value;
     let country;
 
-    if (input === "USA") {
+    switch(input)
+    {
+        case "Canada":
+            break;
+        case "Finland":
+            break;
+        case "Haiti":
+            break;
+        case "Mexico":
+            break;
+        case "Rwanda":
+            break;
+        case "Uganda":
+            break;
+        case "Ukraine":
+            break;     
+        case "Venezuela":
+            break;       
+    }
+
+    if (input === "Canada") {
         //set country to usa 
-        country = usa;
+        country = canada;
     }
     else if (input === "Mexico") {
         country = mexico;
     }
     else if (input === "Algeria") {
         country = algeria;
-    }
+    } 
 
 }
