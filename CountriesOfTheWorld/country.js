6a3dacb40f8da6bb8ccc3f89a1ebac125e839b41
ktMcD <geek.katie@gmail.com>
2023-02-09 05:10:01 -0800

    elements and html / js updates
    To do: define inner html and inner text in the javascript function
    set up the images in the inner html

--- CountriesOfTheWorld/country.js
+++ CountriesOfTheWorld/country.js
@@ -7,14 +7,14 @@ class Country {
     }
 }
 
-let canada = new Country("Canada", "French and English", "Hello, World, eh?", ["red", "white", "blue"]);
-let finland = new Country("Finland", "Finnish", "'Sup", ["blue", "yellow", "red"])
-let haiti = new Country("Haiti", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
-let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
-let rwanda = new Country("Rwanda", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
-let uganda = new Country("Uganda", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
-let ukraine = new Country("Ukraine", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
-let venezuela = new Country("Venezuela", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
+let canada = new Country("Canada", "French and English", "Hello, World, eh?", ["#EF3340", "#FFFFFF", ""]);
+let finland = new Country("Finland", "Finnish", "Hei maailma", ["#002F6C", "#FFFFFF", "#ffffff"])
+let haiti = new Country("Haiti", "French and Haitian Creole", "Bonjour le monde", ["#EF3340", "#003DA5", "#ffffff"])
+let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["#006341", "#FFFFFF", "#C8102E"]);
+let rwanda = new Country("Rwanda", "Kinyarwanda, French, Swahili, English", "Smuraho isi", ["#0077C8", "#FFD100", "#007749"])
+let uganda = new Country("Uganda", "English nad Swahili", "Salamu, Dunia", ["#FFD100", "#EF3340", "#000000"])
+let ukraine = new Country("Ukraine", "Arabic and Berber", "Dobryy den’	svit", ["#0057b7", "#ffd700", "#ffffff"])
+let venezuela = new Country("Venezuela", "Espanol", "Hola Mundo!", ["#FCE300", "#003DA5", "#EF3340"])
 
 function SwitchCountry() {
     let input = document.querySelector('#CountryList').value;
@@ -44,11 +44,25 @@ function SwitchCountry() {
         //set country to usa 
         country = canada;
     }
+    else if (input === "Finland") {
+        country = finland;
+    }
+    else if (input === "Haiti") {
+        country = haiti;
+    } 
     else if (input === "Mexico") {
         country = mexico;
-    }
-    else if (input === "Algeria") {
-        country = algeria;
     } 
-
+    else if (input === "Rwanda") {
+        country = rwanda;
+    } 
+    else if (input === "Uganda") {
+        country = uganda;
+    } 
+    else if (input === "Ukraine") {
+        country = ukraine;
+    } 
+    else if (input === "Venezuela") {
+        country = venezuela;
+    } 
 }
